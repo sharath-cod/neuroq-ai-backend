de9b866088a61a5122a86f15f71dfd78a3601b16
Sharath <sharathsudhakara21@gmail.com>
2026-05-01 11:33:42 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ const multer       = require('multer');
 require('dotenv').config();
 
 const app  = express();
+app.set('trust proxy', 1);
 const PORT = process.env.PORT || 5000;
 
 // ─── Security middleware ──────────────────────
